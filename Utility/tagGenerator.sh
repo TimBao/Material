43327b7a84f41c89b49546bd2c4e13adb0830bf4
tbao <Tim@TimBaos-Mac-mini.local>
2013-09-12 12:57:51 +0800

    update tags_generator.sh

--- Utility/tagGenerator.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-
-CURRENT_PATH=
-BRANCH=
-PROJECT_PATH=
-
-ctags -R -o ~/.vim/ctags/code_tags --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q PROJECT_PATH
---
+++ Utility/tags_generator.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+CURRENT_PATH=
+BRANCH=
+PROJECT_PATH=
+
+ctags -R -o ~/.vim/ctags/code_tags --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q $PROJECT_PATH
